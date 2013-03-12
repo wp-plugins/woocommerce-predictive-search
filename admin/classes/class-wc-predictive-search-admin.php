6625580bb4e3fec92cb037a33b6abeb88b94def0
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-12 05:28:01 +0000

    update the link of Woocommerce Email Inquiry and Cart options plugin
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-predictive-search/trunk@680275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/classes/class-wc-predictive-search-admin.php
+++ admin/classes/class-wc-predictive-search-admin.php
@@ -496,7 +496,7 @@ class WC_Predictive_Search_Settings {
 		$html .= '<li>* <a href="http://wordpress.org/extend/plugins/woocommerce-predictive-search/" target="_blank">'.__('WooCommerce Predictive Search', 'woops').'</a></li>';
 		$html .= '<li>* <a href="http://wordpress.org/extend/plugins/woocommerce-compare-products/" target="_blank">'.__('WooCommerce Compare Products', 'woops').'</a></li>';
 		$html .= '<li>* <a href="http://wordpress.org/extend/plugins/woo-widget-product-slideshow/" target="_blank">'.__('WooCommerce Widget Product Slideshow', 'woops').'</a></li>';
-		$html .= '<li>* <a href="http://a3rev.com/shop/woocommerce-email-inquiry-and-cart-options/" target="_blank">'.__('WooCommerce Email Inquiry & Cart Options', 'woops').'</a>'.__(' - Pro Version only from a3rev', 'woops').'</li>';
+		$html .= '<li>* <a href="http://wordpress.org/extend/plugins/woocommerce-email-inquiry-cart-options/" target="_blank">'.__('WooCommerce Email Inquiry & Cart Options', 'woops').'</a></li>';
 		$html .= '</ul>';
 		$html .= '</p>';
 		$html .= '<h3>'.__('WordPress Plugins', 'woops').'</h3>';
