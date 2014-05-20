c7627fdd5b12b9bcc9b12fd2623583aef53a02fc
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-20 09:21:20 +0000

    Created tag 2.2.2.2
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-predictive-search/tags/2.2.2.2@917748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin-ui.php
+++ admin/admin-ui.php
@@ -122,15 +122,15 @@ class WC_Predictive_Search_Admin_UI
 	/* upgrade_top_message() */
 	/* Show upgrade top message for pro fields
 	/*-----------------------------------------------------------------------------------*/
-	public function upgrade_top_message( $echo = false ) {
+	public function upgrade_top_message( $echo = false, $setting_id = '' ) {
 		$upgrade_top_message = sprintf( '<div class="pro_feature_top_message">' 
-			. __( 'Settings inside this yellow border are %s Features.', 'wc_email_inquiry' ) 
+			. __( 'Settings inside this yellow border are %s Features.', 'woops' ) 
 			. '<br />' 
-			. __( 'Upgrade to the <a href="%s" target="_blank">%s</a> to activate these settings.', 'wc_email_inquiry' ) 
+			. __( 'Upgrade to the <a href="%s" target="_blank">%s</a> to activate these settings.', 'woops' ) 
 			. '</div>'
-			, apply_filters( $this->plugin_name . '_pro_version_name', __( 'Pro Version', 'wc_email_inquiry' ) )
-			, apply_filters( $this->plugin_name . '_pro_plugin_page_url', $this->pro_plugin_page_url )
-			, apply_filters( $this->plugin_name . '_pro_version_name', __( 'Pro Version', 'wc_email_inquiry' ) ) 
+			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_version_name', apply_filters( $this->plugin_name . '_pro_version_name', __( 'Pro Version', 'woops' ) ) )
+			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_plugin_page_url', apply_filters( $this->plugin_name . '_pro_plugin_page_url', $this->pro_plugin_page_url ) )
+			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_version_name', apply_filters( $this->plugin_name . '_pro_version_name', __( 'Pro Version', 'woops' ) ) )
 		);
 		
 		$upgrade_top_message = apply_filters( $this->plugin_name . '_upgrade_top_message', $upgrade_top_message );
