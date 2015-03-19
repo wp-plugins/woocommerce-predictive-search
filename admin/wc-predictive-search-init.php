c647f0d6496d6f8d5c46aa9e50c60ec18b933a1e
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-19 09:26:04 +0000

    Created tag 2.2.5.
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-predictive-search/tags/2.2.5@1115906 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/wc-predictive-search-init.php
+++ admin/wc-predictive-search-init.php
@@ -1,42 +1,46 @@
 <?php
+
 /**
  * Register Activation Hook
  */
 update_option('wc_predictive_search_plugin', 'woo_predictive_search');
-function wc_predictive_install() {
-	WC_Predictive_Search::create_page( 'woocommerce-search' , 'woocommerce_search_page_id', __('Woocommerce Predictive Search', 'woops'), '[woocommerce_search]' );
-	
-	// Set Settings Default from Admin Init
-	global $wc_predictive_search_admin_init;
-	$wc_predictive_search_admin_init->set_default_settings();
-	update_option('wc_predictive_search_lite_version', '2.2.2.1');
-	flush_rewrite_rules();
-	
-	update_option('wc_predictive_search_just_installed', true);
+function wc_predictive_install()
+{
+    WC_Predictive_Search::create_page('woocommerce-search', 'woocommerce_search_page_id', __('Woocommerce Predictive Search', 'woops'), '[woocommerce_search]');
+
+    // Set Settings Default from Admin Init
+    global $wc_predictive_search_admin_init;
+    $wc_predictive_search_admin_init->set_default_settings();
+    update_option('wc_predictive_search_lite_version', '2.2.5');
+    flush_rewrite_rules();
+
+    update_option('wc_predictive_search_just_installed', true);
 }
 
-function woops_init() {
-	if ( get_option('wc_predictive_search_just_installed') ) {
-		delete_option('wc_predictive_search_just_installed');
-		wp_redirect( admin_url( 'admin.php?page=woo-predictive-search', 'relative' ) );
-		exit;
-	}
-	load_plugin_textdomain( 'woops', false, WOOPS_FOLDER.'/languages' );
+function woops_init()
+{
+    if (get_option('wc_predictive_search_just_installed')) {
+        delete_option('wc_predictive_search_just_installed');
+        wp_redirect(admin_url('admin.php?page=woo-predictive-search', 'relative'));
+        exit;
+    }
+    load_plugin_textdomain('woops', false, WOOPS_FOLDER . '/languages');
 }
 
 // Add language
 add_action('init', 'woops_init');
 
 // Add custom style to dashboard
-add_action( 'admin_enqueue_scripts', array( 'WC_Predictive_Search_Hook_Filter', 'a3_wp_admin' ) );
+add_action('admin_enqueue_scripts', array('WC_Predictive_Search_Hook_Filter', 'a3_wp_admin'));
 
-add_action( 'plugins_loaded', array( 'WC_Predictive_Search_Hook_Filter', 'plugins_loaded' ), 8 );
+add_action('plugins_loaded', array('WC_Predictive_Search_Hook_Filter', 'plugins_loaded'), 8);
 
 // Add text on right of Visit the plugin on Plugin manager page
-add_filter( 'plugin_row_meta', array('WC_Predictive_Search_Hook_Filter', 'plugin_extra_links'), 10, 2 );
+add_filter('plugin_row_meta', array('WC_Predictive_Search_Hook_Filter', 'plugin_extra_links'), 10, 2);
 
-function register_widget_woops_predictive_search() {
-	register_widget('WC_Predictive_Search_Widgets');
+function register_widget_woops_predictive_search()
+{
+    register_widget('WC_Predictive_Search_Widgets');
 }
 
 // Need to call Admin Init to show Admin UI
@@ -44,10 +48,10 @@ global $wc_predictive_search_admin_init;
 $wc_predictive_search_admin_init->init();
 
 // Add upgrade notice to Dashboard pages
-add_filter( $wc_predictive_search_admin_init->plugin_name . '_plugin_extension', array( 'WC_Predictive_Search', 'plugin_extension' ) );
+add_filter($wc_predictive_search_admin_init->plugin_name . '_plugin_extension', array('WC_Predictive_Search', 'plugin_extension'));
 
 // Custom Rewrite Rules
-add_action('init', array('WC_Predictive_Search_Hook_Filter', 'custom_rewrite_rule'), 101 );
+add_action('init', array('WC_Predictive_Search_Hook_Filter', 'custom_rewrite_rule'), 101);
 
 // Registry widget
 add_action('widgets_init', 'register_widget_woops_predictive_search');
@@ -56,23 +60,23 @@ add_action('widgets_init', 'register_widget_woops_predictive_search');
 add_shortcode('woocommerce_search', array('WC_Predictive_Search_Shortcodes', 'parse_shortcode_search_result'));
 
 // Add Predictive Search Meta Box to all post type
-add_action( 'add_meta_boxes', array('WC_Predictive_Search_Meta','create_custombox'), 9 );
+add_action('add_meta_boxes', array('WC_Predictive_Search_Meta', 'create_custombox'), 9);
 
 // Save Predictive Search Meta Box to all post type
-if(in_array(basename($_SERVER['PHP_SELF']), array('post.php', 'page.php', 'page-new.php', 'post-new.php'))){
-	add_action( 'save_post', array('WC_Predictive_Search_Meta','save_custombox' ) );
+if (in_array(basename($_SERVER['PHP_SELF']), array('post.php', 'page.php', 'page-new.php', 'post-new.php'))) {
+    add_action('save_post', array('WC_Predictive_Search_Meta', 'save_custombox'));
 }
 
 // Add search widget icon to Page Editor
-if (in_array (basename($_SERVER['PHP_SELF']), array('post.php', 'page.php', 'page-new.php', 'post-new.php') ) ) {
-	add_action('media_buttons_context', array('WC_Predictive_Search_Shortcodes', 'add_search_widget_icon') );
-	add_action('admin_footer', array('WC_Predictive_Search_Shortcodes', 'add_search_widget_mce_popup'));
+if (in_array(basename($_SERVER['PHP_SELF']), array('post.php', 'page.php', 'page-new.php', 'post-new.php'))) {
+    add_action('media_buttons_context', array('WC_Predictive_Search_Shortcodes', 'add_search_widget_icon'));
+    add_action('admin_footer', array('WC_Predictive_Search_Shortcodes', 'add_search_widget_mce_popup'));
 }
 
 if (!is_admin()) {
-	add_filter( 'posts_search', array('WC_Predictive_Search_Hook_Filter', 'search_by_title_only'), 500, 2 );
-	add_filter( 'posts_orderby', array('WC_Predictive_Search_Hook_Filter', 'predictive_posts_orderby'), 500, 2 );
-	add_filter( 'posts_request', array('WC_Predictive_Search_Hook_Filter', 'posts_request_unconflict_role_scoper_plugin'), 500, 2);
+    add_filter('posts_search', array('WC_Predictive_Search_Hook_Filter', 'search_by_title_only'), 500, 2);
+    add_filter('posts_orderby', array('WC_Predictive_Search_Hook_Filter', 'predictive_posts_orderby'), 500, 2);
+    add_filter('posts_request', array('WC_Predictive_Search_Hook_Filter', 'posts_request_unconflict_role_scoper_plugin'), 500, 2);
 }
 
 // AJAX get result search page
@@ -83,21 +87,19 @@ add_action('wp_ajax_nopriv_woops_get_result_search_page', array('WC_Predictive_S
 add_action('wp_ajax_woops_get_result_popup', array('WC_Predictive_Search_Hook_Filter', 'get_result_popup'));
 add_action('wp_ajax_nopriv_woops_get_result_popup', array('WC_Predictive_Search_Hook_Filter', 'get_result_popup'));
 
-if ( ! is_admin() )
-	add_action('init',array('WC_Predictive_Search_Hook_Filter','add_frontend_style'));
+if (!is_admin()) add_action('init', array('WC_Predictive_Search_Hook_Filter', 'add_frontend_style'));
 
 // Check upgrade functions
 add_action('plugins_loaded', 'woo_ps_lite_upgrade_plugin');
-function woo_ps_lite_upgrade_plugin () {
-	
-	// Upgrade to 1.0.9
-	if(version_compare(get_option('wc_predictive_search_lite_version'), '2.0') === -1){
-		WC_Predictive_Search::upgrade_version_2_0();
-		update_option('wc_predictive_search_lite_version', '2.0');
-	}
-	
-	update_option('wc_predictive_search_lite_version', '2.2.2.1');
-	
-}
+function woo_ps_lite_upgrade_plugin()
+{
 
+    // Upgrade to 1.0.9
+    if (version_compare(get_option('wc_predictive_search_lite_version'), '2.0') === -1) {
+        WC_Predictive_Search::upgrade_version_2_0();
+        update_option('wc_predictive_search_lite_version', '2.0');
+    }
+
+    update_option('wc_predictive_search_lite_version', '2.2.5');
+}
 ?>
--- wc-predictive-search.php
+++ wc-predictive-search.php
@@ -3,11 +3,11 @@
 Plugin Name: WooCommerce Predictive Search LITE
 Plugin URI: http://a3rev.com/shop/woocommerce-predictive-search/
 Description: With WooCommerce Predictive Search Lite you can add an awesome Predictive Products Search widget to any widgetized area on your site.
-Version: 2.2.2.1
+Version: 2.2.5
 Author: A3 Revolution
 Author URI: http://www.a3rev.com/
-Requires at least: 3.5
-Tested up to: 3.8.1
+Requires at least: 3.7
+Tested up to: 4.1
 License: GPLv2 or later
 
 	WooCommerce Predictive Search. Plugin for the WooCommerce plugin.
@@ -21,25 +21,23 @@ License: GPLv2 or later
 */
 ?>
 <?php
-define( 'WOOPS_FILE_PATH', dirname(__FILE__) );
-define( 'WOOPS_DIR_NAME', basename(WOOPS_FILE_PATH) );
-define( 'WOOPS_FOLDER', dirname(plugin_basename(__FILE__)) );
-define( 'WOOPS_NAME', plugin_basename(__FILE__) );
-define( 'WOOPS_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
-define( 'WOOPS_JS_URL',  WOOPS_URL . '/assets/js' );
-define( 'WOOPS_CSS_URL',  WOOPS_URL . '/assets/css' );
-define( 'WOOPS_IMAGES_URL',  WOOPS_URL . '/assets/images' );
-if(!defined("WOOPS_AUTHOR_URI"))
-    define("WOOPS_AUTHOR_URI", "http://a3rev.com/shop/woocommerce-predictive-search/");
-if(!defined("WOO_PREDICTIVE_SEARCH_DOCS_URI"))
-    define("WOO_PREDICTIVE_SEARCH_DOCS_URI", "http://docs.a3rev.com/user-guides/woocommerce/woo-predictive-search/");
+define('WOOPS_FILE_PATH', dirname(__FILE__));
+define('WOOPS_DIR_NAME', basename(WOOPS_FILE_PATH));
+define('WOOPS_FOLDER', dirname(plugin_basename(__FILE__)));
+define('WOOPS_NAME', plugin_basename(__FILE__));
+define('WOOPS_URL', untrailingslashit(plugins_url('/', __FILE__)));
+define('WOOPS_JS_URL', WOOPS_URL . '/assets/js');
+define('WOOPS_CSS_URL', WOOPS_URL . '/assets/css');
+define('WOOPS_IMAGES_URL', WOOPS_URL . '/assets/images');
+if (!defined("WOOPS_AUTHOR_URI")) define("WOOPS_AUTHOR_URI", "http://a3rev.com/shop/woocommerce-predictive-search/");
+if (!defined("WOO_PREDICTIVE_SEARCH_DOCS_URI")) define("WOO_PREDICTIVE_SEARCH_DOCS_URI", "http://docs.a3rev.com/user-guides/woocommerce/woo-predictive-search/");
 
-include('admin/admin-ui.php');
-include('admin/admin-interface.php');
+include ('admin/admin-ui.php');
+include ('admin/admin-interface.php');
 
-include('admin/admin-pages/predictive-search-page.php');
+include ('admin/admin-pages/predictive-search-page.php');
 
-include('admin/admin-init.php');
+include ('admin/admin-init.php');
 
 include 'classes/class-wc-predictive-search-filter.php';
 include 'classes/class-wc-predictive-search.php';
@@ -53,52 +51,57 @@ include 'tinymce3/tinymce.php';
 include 'admin/wc-predictive-search-init.php';
 
 /**
-* Call when the plugin is activated
-*/
-register_activation_hook(__FILE__,'wc_predictive_install');
+ * Call when the plugin is activated
+ */
+register_activation_hook(__FILE__, 'wc_predictive_install');
 
-function wc_predictive_uninstall() {
-	if ( get_option('woocommerce_search_lite_clean_on_deletion') == 'yes' ) {
-		delete_option('woocommerce_search_text_lenght');
-		delete_option('woocommerce_search_result_items');
-		delete_option('woocommerce_search_sku_enable');
-		delete_option('woocommerce_search_price_enable');
-		delete_option('woocommerce_search_addtocart_enable');
-		delete_option('woocommerce_search_categories_enable');
-		delete_option('woocommerce_search_tags_enable');
-		delete_option('woocommerce_search_box_text');
-		delete_option('woocommerce_search_page_id');
-		delete_option('woocommerce_search_exclude_products');
-		
-		delete_option('woocommerce_search_exclude_p_categories');
-		delete_option('woocommerce_search_exclude_p_tags');
-		delete_option('woocommerce_search_exclude_posts');
-		delete_option('woocommerce_search_exclude_pages');
-		delete_option('woocommerce_search_focus_enable');
-		delete_option('woocommerce_search_focus_plugin');
-		delete_option('woocommerce_search_product_items');
-		delete_option('woocommerce_search_p_sku_items');
-		delete_option('woocommerce_search_p_cat_items');
-		delete_option('woocommerce_search_p_tag_items');
-		delete_option('woocommerce_search_post_items');
-		delete_option('woocommerce_search_page_items');
-		delete_option('woocommerce_search_character_max');
-		delete_option('woocommerce_search_width');
-		delete_option('woocommerce_search_padding_top');
-		delete_option('woocommerce_search_padding_bottom');
-		delete_option('woocommerce_search_padding_left');
-		delete_option('woocommerce_search_padding_right');
-		delete_option('woocommerce_search_custom_style');
-		delete_option('woocommerce_search_global_search');
-		
-		delete_option('woocommerce_search_lite_clean_on_deletion');
-		
-		delete_post_meta_by_key('_predictive_search_focuskw');
-		
-		wp_delete_post( get_option('woocommerce_search_page_id') , true );
-	}
+function wc_predictive_uninstall()
+{
+    if (get_option('woocommerce_search_lite_clean_on_deletion') == 'yes') {
+        delete_option('woocommerce_search_text_lenght');
+        delete_option('woocommerce_search_result_items');
+        delete_option('woocommerce_search_sku_enable');
+        delete_option('woocommerce_search_price_enable');
+        delete_option('woocommerce_search_addtocart_enable');
+        delete_option('woocommerce_search_categories_enable');
+        delete_option('woocommerce_search_tags_enable');
+        delete_option('woocommerce_search_box_text');
+        delete_option('woocommerce_search_page_id');
+        delete_option('woocommerce_search_exclude_products');
+
+        delete_option('woocommerce_search_exclude_p_categories');
+        delete_option('woocommerce_search_exclude_p_tags');
+        delete_option('woocommerce_search_exclude_posts');
+        delete_option('woocommerce_search_exclude_pages');
+        delete_option('woocommerce_search_focus_enable');
+        delete_option('woocommerce_search_focus_plugin');
+        delete_option('woocommerce_search_product_items');
+        delete_option('woocommerce_search_p_sku_items');
+        delete_option('woocommerce_search_p_cat_items');
+        delete_option('woocommerce_search_p_tag_items');
+        delete_option('woocommerce_search_post_items');
+        delete_option('woocommerce_search_page_items');
+        delete_option('woocommerce_search_character_max');
+        delete_option('woocommerce_search_width');
+        delete_option('woocommerce_search_padding_top');
+        delete_option('woocommerce_search_padding_bottom');
+        delete_option('woocommerce_search_padding_left');
+        delete_option('woocommerce_search_padding_right');
+        delete_option('woocommerce_search_custom_style');
+        delete_option('woocommerce_search_global_search');
+
+        delete_option('woocommerce_search_enable_google_analytic');
+        delete_option('woocommerce_search_google_analytic_id');
+        delete_option('woocommerce_search_google_analytic_query_parameter');
+
+        delete_option('woocommerce_search_lite_clean_on_deletion');
+
+        delete_post_meta_by_key('_predictive_search_focuskw');
+
+        wp_delete_post(get_option('woocommerce_search_page_id'), true);
+    }
 }
-if ( get_option('woocommerce_search_lite_clean_on_deletion') == 'yes' ) {
-	register_uninstall_hook( __FILE__, 'wc_predictive_uninstall' );
+if (get_option('woocommerce_search_lite_clean_on_deletion') == 'yes') {
+    register_uninstall_hook(__FILE__, 'wc_predictive_uninstall');
 }
 ?>
